a2b3a011225de2482027766026446de89c6fdaff
Alexander Austin <austinag@g.cofc.edu>
2018-04-23 10:35:37 -0400

    Refactoring applied to non-streaming lobby

--- app/src/main/java/com/example/alex/tuneup/v_lobby.java
+++ app/src/main/java/com/example/alex/tuneup/v_lobby.java
@@ -31,6 +31,7 @@ public class v_lobby extends Activity {
     private TextView songInfo, lobbyName;
     private String key, trackInfo, numMembers;
     private JSONObject currentTrack, nextTrack;    //This lobby will not stream, therefore, we only need to have the JSON object of the current track
+    private RequestManager r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,48 +66,65 @@ public class v_lobby extends Activity {
         songInfo = findViewById(R.id.song_info);
         lobbyName = findViewById(R.id.lobby_name);
         RequestManager r = new RequestManager();
+
+        populateLobby();
+
+
+        //Declare listeners for all Buttons
+        backButton.setOnClickListener(buttonListener);
+        searchButton.setOnClickListener(buttonListener);
+        viewQueue.setOnClickListener(buttonListener);
+
+    }
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i;
+            switch (v.getId()) {
+                case R.id.bAddSong:
+                    i = new Intent(getApplicationContext(), v_search.class);
+                    i.putExtra("lobbyKey", key);
+                    startActivity(i);
+                    break;
+
+                case R.id.bViewQueue:
+                    // Toast.makeText(lobby_streamHidden.this, "Hello", Toast.LENGTH_LONG).show();
+                    i = new Intent(getApplicationContext(), v_viewQueue.class);
+                    i.putExtra("lobbyKey", key);
+                    startActivity(i);
+                    break;
+
+                case R.id.bBack:
+                    i = new Intent(getApplicationContext(), v_home.class);
+                    i.putExtra("lobbyKey", key);
+                    startActivity(i);
+                    break;
+            }
+
+        }
+
+
+    };
+
+
+
+    protected void populateLobby() {
         try {
             r.web_lobbyGetData(key);
+
+            //Populate data, this maybe could use some refactoring, since we are declaring a track object that can hold some of these values
             name = r.loc_lobbyName();
             lobbyName.setText(URLDecoder.decode(name, "UTF-8"));
-            trackInfo = URLDecoder.decode((r.loc_lobbyPlaying("name") + " - " + r.loc_lobbyPlaying("artist")) , "UTF-8");
+            trackInfo = URLDecoder.decode((r.loc_lobbyPlaying("name") + " - " + r.loc_lobbyPlaying("artist")), "UTF-8");
             songInfo.setText(trackInfo);
             Bitmap img = new GetAlbumArt().execute(r.loc_lobbyPlaying("artwork")).get();
             albumCover.setImageBitmap(img);
             numMembers = r.loc_lobbyMembersCount();
-
-        }catch(Exception e){
-            Log.i("Error loading lobby" , e.getMessage());
+        } catch (Exception e) {
+            Log.i("Error loading lobby", e.getMessage());
             Toast.makeText(v_lobby.this, "Error Loading Lobby Data", Toast.LENGTH_LONG).show();
         }
-
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), v_home.class);
-                startActivity(i);
-            }
-        });
-
-
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), v_search.class);
-                startActivity(i);
-            }
-        });
-
-        viewQueue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v_lobby.this, "Hello", Toast.LENGTH_LONG).show();
-                Intent i = new Intent(getApplicationContext(), v_search.class);
-                startActivity(i);
-            }
-        });
-
     }
 
 }
